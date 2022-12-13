037a69ada7ff51f0b03ac45aa499b4e8430b8b87
Olerons <ser1a@ya.ru>
2022-12-13 17:42:41 +0300

    class hero

---
+++ hero.py
@@ -0,0 +1,10 @@
+import pygame as pg
+from constants import *
+from helper_def import load_img
+
+class Hero(pg.sprite.Sprite):
+    def __init__(self, group):
+        super().__init__(group)
+        img_sprite = load_img('appear.png', 'hero')
+
+
